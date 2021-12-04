ab6e42441bd0e62547a0e4f7e9d9478acc4c379b
= <=>
2021-12-04 12:03:12 +0530

    making pie chart with dummmy data and ordinal scale

--- graph.js
+++ graph.js
@@ -1,6 +1,40 @@
+// graph dims
+let dims = { height: 300, width: 400, radius: 150 };
+
 const container = d3.select(".graph-container");
 
-const svg = container.append("svg").attr("height", 500).attr("width", 600);
+const svg = container
+  .append("svg")
+  .attr("height", dims.height + 150)
+  .attr("width", dims.width + 150);
+
+const graph = svg
+  .append("g")
+  .attr("height", dims.height)
+  .attr("width", dims.width)
+  .attr(
+    "transform",
+    `translate(${(dims.width + 150) / 2}, ${(dims.height + 150) / 2})`
+  );
+
+const pie = d3
+  .pie()
+  .sort(null)
+  .value((d) => d);
+
+const arc = d3.arc().outerRadius(dims.radius).innerRadius(0);
+
+const colorScale = d3.scaleOrdinal(d3["schemeSet3"]).domain([1, 2, 3, 4, 5]);
+
+graph
+  .selectAll("path")
+  .data(pie([1, 2, 3, 4, 5]))
+  .enter()
+  .append("path")
+  .attr("d", (d) => arc(d))
+  .attr("stroke", "black")
+  .attr("stroke-width", 3)
+  .attr("fill", (d) => colorScale(d));
 
 let render = (values) => {
   console.log(values);
@@ -24,5 +58,5 @@ db.collection("investment").onSnapshot((res) => {
     }
   });
 
-  render(values);
+  //   render(values);
 });
